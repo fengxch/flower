1ef7407f7dfe9161db908f9f012e7ea727117829
fengxiaochuan <fengxiaochuan@xiaomi.com>
2018-12-18 19:43:22 +0800

    first commit
    Signed-off-by: fengxiaochuan <fengxiaochuan@xiaomi.com>

---
+++ app/src/main/java/flower/fengliu/beijing/cn/flower/MainActivity.java
@@ -0,0 +1,32 @@
+package flower.fengliu.beijing.cn.flower;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.qmuiteam.qmui.widget.dialog.QMUIDialog;
+import com.qmuiteam.qmui.widget.dialog.QMUIDialogAction;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new QMUIDialog.MessageDialogBuilder(this)
+                .setMessage("确认下载此文件？")
+                .addAction("取消", new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                })
+                .addAction("确认", new QMUIDialogAction.ActionListener() {
+                    @Override
+                    public void onClick(QMUIDialog dialog, int index) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
+    }
+
+}
